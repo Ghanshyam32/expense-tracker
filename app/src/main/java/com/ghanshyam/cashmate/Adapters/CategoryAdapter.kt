d3daf112f3297466968ff0e2797eab4949347ede
Ghanshyam32 <ghanshyammishra3205615@gmail.com>
2023-10-20 23:45:24 +0530

    category adapter

--- app/src/main/java/com/ghanshyam/cashmate/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/ghanshyam/cashmate/Adapters/CategoryAdapter.kt
@@ -1,8 +1,45 @@
 package com.ghanshyam.cashmate.Adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.ghanshyam.cashmate.Models.Category
+import com.ghanshyam.cashmate.R
 
-class CategoryAdapter {
+class CategoryAdapter(
+    private val context: Context,
+    private val categories: ArrayList<Category>,
+    private val categoryClickListener: CategoryClickListener
+) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
+    interface CategoryClickListener {
+        fun onCategoryClicked(category: Category)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.sample_category, parent, false)
+        return CategoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = categories[position]
+        holder.bind(category)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        // Define views and initialize them here
+
+        fun bind(category: Category) {
+            // Bind data to views in the ViewHolder here
+            itemView.setOnClickListener {
+                categoryClickListener.onCategoryClicked(category)
+            }
+        }
+    }
 }
